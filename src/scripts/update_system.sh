29db63d8dea47dc4ec384b1d8cc5a248af8f0b8d
TechXero <steve@techxero.com>
2022-10-15 16:36:22 +0300

    Nuke!!!

--- src/scripts/update_system.sh
+++ src/scripts/update_system.sh
@@ -1,30 +1,93 @@
-#!/usr/bin/bash
-set -e
-
-echo "Select which do you want to update."
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	DarkXero
+# Website 	: 	http://xerolinux.xyz
+##################################################################################################################
 echo
-echo "1. Update Arch / AUR & Flatpaks (yay && flatpak update)"
-echo "2. Update Everything (Arch / AUR / Flatpaks / Git Repos / Docker / nix etc...) (Topgrade)"
-echo "3. Exit"
+tput setaf 3
+echo "###############################################################################"
+echo "#                          XeroLinux System Updater.                          #"
+echo "#                                                                             #"
+echo "#                            !!!! ATTENTION !!!                               #"
+echo "#                                                                             #"
+echo "#           Option 3) uses Topgrade which will Update *EVERYTHING*.           #"
+echo "# Which includes any Git Repos, Docker Containers, Nix, Device Firmware etc.. #"
+echo "#                                                                             #"
+echo "#               Do *NOT* use unless you know what you are doing               #"
+echo "###############################################################################"
+tput sgr0
+echo
+echo "Select which packages you want to update."
+echo
+echo "########## Standard Users ##########"
+echo
+echo "1.  Arch & AUR Packages."
+echo "2.  Flatpak Packages."
+echo
+echo "########## Advanced Users ##########"
+echo
+echo "3.  Everything, One Shot. (Nuke!)"
+echo
+echo "Type Your Selection..."
 echo
 
-read -p 'Select option : ' CHOICE
+read CHOICE
 
 case $CHOICE in
-  1)
-    yay -Syu
-    flatpak update
-    echo " --- Done --- "
-    ;;
-  2)
-    topgrade
-    echo " --- Done --- "
-    ;;
-  3)
-    echo
-    echo "You can close this window now"
-    ;;
-  *)
-    echo "Invalid option!"
-    ;;
+
+    1 )
+      echo
+      echo "##########################################"
+      echo "       Updating Arch & AUR Packages       "
+      echo "##########################################"
+			sleep 3
+			if [[ -f "/usr/bin/paru" ]]; then
+               paru -Syu
+             else
+               yay -Syu
+            fi
+			sleep 3
+      echo "#######################################"
+      echo "                 Done !                "
+      echo "#######################################"
+            clear && sh /usr/share/xerowelcome/scripts/update_system.sh
+      ;;
+
+    2 )
+      echo
+      echo "##########################################"
+      echo "        Updating Flatpak Packages         "
+      echo "##########################################"
+			sleep 3
+			flatpak update
+			sleep 3
+      echo "#######################################"
+      echo "                 Done !                "
+      echo "#######################################"
+            clear && sh /usr/share/xerowelcome/scripts/update_system.sh
+      ;;
+
+
+    3 )
+      echo
+      echo "##########################################"
+      echo "       !!! Updating Everything !!!        "
+      echo "##########################################"
+			sleep 3
+			topgrade
+			sleep 3
+      echo "#######################################"
+      echo "                 Done !                "
+      echo "#######################################"
+			clear && sh /usr/share/xerowelcome/scripts/update_system.sh
+
+      ;;
+
+    * )
+      echo "#################################"
+      echo "Choose the correct number"
+      echo "#################################"
+      ;;
 esac
